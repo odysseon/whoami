24a8828974e441112bad0ef70bbf548f1d4de61d
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2026-04-28 00:39:44 +0100

    chore: ignore linting generated files

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,10 @@
-const tseslint = require('typescript-eslint');
-const prettierPlugin = require('eslint-plugin-prettier');
-const prettierConfig = require('eslint-config-prettier');
+const tseslint = require("typescript-eslint");
+const prettierPlugin = require("eslint-plugin-prettier");
+const prettierConfig = require("eslint-config-prettier");
 
 module.exports = tseslint.config(
   {
-    files: ['**/*.ts'],
+    files: ["**/*.ts"],
     extends: [...tseslint.configs.recommended],
     plugins: { prettier: prettierPlugin },
     languageOptions: {
@@ -14,17 +14,24 @@ module.exports = tseslint.config(
       },
     },
     rules: {
-      'prettier/prettier': 'error',
-      '@typescript-eslint/no-explicit-any': 'error',
-      '@typescript-eslint/explicit-function-return-type': 'error',
-      '@typescript-eslint/explicit-module-boundary-types': 'error',
-      '@typescript-eslint/interface-name-prefix': 'off',
-      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-      '@typescript-eslint/return-await': ['error', 'always'],
+      "prettier/prettier": "error",
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/explicit-function-return-type": "error",
+      "@typescript-eslint/explicit-module-boundary-types": "error",
+      "@typescript-eslint/interface-name-prefix": "off",
+      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+      "@typescript-eslint/return-await": ["error", "always"],
     },
   },
   prettierConfig,
   {
-    ignores: ['**/dist/**', '**/node_modules/**', '**/coverage/**'],
-  }
+    ignores: [
+      "**/dist/**",
+      "**/node_modules/**",
+      "**/coverage/**",
+      "**/generated/**",
+      "**/*.generated.ts",
+      "**/prisma/**",
+    ],
+  },
 );
